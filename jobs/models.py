a5ffd645980d226c8b9167440590d474a7416dad
Dan Collins <dcollins31@gmail.com>
2018-12-09 00:24:06 -0600

    migrated

--- jobs/models.py
+++ jobs/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+# Pyhon needs to find the job model
+
 
 class Job(models.Model):
-    image = models.ImageField(upload_to='')
+    image = models.ImageField(upload_to='images/')
+    summary = models.CharField(max_length=200)
